66b7720ac83dbfd689256e7b87b6ef99b569693a
陈惊蛰 <crazor@foxmail.com>
2018-07-09 22:34:48 +0800

    init

---
+++ SimpleStoreApplication/Common/IShoppingCartService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IShoppingCartService
+    {
+        [OperationContract]
+        Task AddItem(ShoppingCartItem item);
+        [OperationContract]
+        Task DeleteItem(string productName);
+        [OperationContract]
+        Task<List<ShoppingCartItem>> GetItems();
+    }
+}
---
+++ SimpleStoreApplication/Common/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common
+{
+    public struct ShoppingCartItem
+    {
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int Amount { get; set; }
+        public double LineTotal { get { return Amount * UnitPrice; } }
+    }
+}
